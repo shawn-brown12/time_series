6cc6c324edd32e3228218e28dd571f68f4fa6734
Shawn.Brown <shawn.c.brown12@gmail.com>
2023-02-08 16:56:58 -0600

    updating notes and exercise files

--- acquire.py
+++ acquire.py
@@ -1,5 +1,10 @@
 import os
+from env import host, username, password
+
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from env import username, host, password
 
@@ -37,3 +42,93 @@ def acquire_store():
         df.to_csv(filename, index=False)
         
         return df
+    
+#------------------------------------------------------
+
+def acquire_germany():
+    
+    if os.path.isfile('germany.csv'):
+        
+        df = pd.read_csv('germany.csv')
+
+        return df
+    
+    else:
+        
+        df = pd.read_csv('https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv')
+        
+        df.to_csv('germany.csv')
+
+        return df
+    
+#------------------------------------------------------
+
+def wrangle_store_data():
+    
+    df = acquire.acquire_store()
+    
+    df['sale_date'] = pd.to_datetime(df['sale_date'], infer_datetime_format=True)
+    df = df.set_index('sale_date').sort_index()
+    
+    df['month'] = df.index.month_name()
+    df['day'] = df.index.day_name()
+    
+    df['sales_total'] = df['sale_amount'] * df['item_price']
+    
+    return df
+
+#------------------------------------------------------
+
+def wrangle_germany():
+    
+    df = acquire_germany()
+    
+    df['Date'] = pd.to_datetime(df['Date'], infer_datetime_format=True)
+    df = df.set_index('Date').sort_index()
+    
+    df['month'] = df.index.month_name()
+    df['year'] = df.index.year_name()
+    
+    df['Wind'] = df['Wind'].fillna(value=0)
+    df['Solar'] = df['Solar'].fillna(value=0)
+    df['Wind+Solar'] = df['Wind+Solar'].fillna(value=0)
+    
+    return df
+
+#------------------------------------------------------
+
+def store_distributions(df):
+    
+    vars_to_plot = ['sale_amount', 'item_price', 'sales_total']
+
+    for col in vars_to_plot:
+    
+        df.groupby('sale_date')[vars_to_plot].sum().plot()
+        
+        plt.show()
+        
+#------------------------------------------------------
+
+def germany_distributions(df):
+    
+    vars_to_plot = ['Consumption', 'Wind', 'Solar', 'Wind+Solar']
+
+    for col in vars_to_plot:
+    
+        df.groupby('Date')[vars_to_plot].sum().plot()
+    
+        plt.show()
+
+#------------------------------------------------------
+
+
+#------------------------------------------------------
+
+
+#------------------------------------------------------
+
+
+#------------------------------------------------------
+
+
+#------------------------------------------------------
